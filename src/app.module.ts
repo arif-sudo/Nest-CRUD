642a8c1952c4a917420c4d932506bd4d101f8500
Arif <arif520gg@gmail.com>
2023-07-28 23:15:37 +0400

    db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarModule } from './car/car/car.module';
+import { UsersModule } from './users/users.module';
 
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CarModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PGHOST,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      ssl: true,
+    }),
+    CarModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
